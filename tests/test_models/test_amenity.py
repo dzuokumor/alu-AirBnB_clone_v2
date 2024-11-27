ceedb46b21b0ea54148e158a84be01211011c759
dzuokumor <d.zuokumor@alustudent.com>
2024-11-27 08:24:46 +0200

    tests

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ """
+from datetime import datetime
 from tests.test_models.test_base_model import TestBaseModel
 from models.amenity import Amenity
+import models
 
 
 class TestAmenity(TestBaseModel):
@@ -13,8 +15,20 @@ class TestAmenity(TestBaseModel):
         self.name = "Amenity"
         self.value = Amenity
 
+    def setUp(self):
+        self.storage = models.storage
+
     def test_name2(self):
-        """ """
         new = self.value()
         new.name = "amenity"
         self.assertEqual(type(new.name), str)
+
+    def test_created_at(self):
+        amenity = Amenity(name="WiFi")
+        amenity.save()
+
+        stored_amenity = self.storage.all(Amenity).get(amenity.id)
+
+        self.assertIsInstance(stored_amenity.created_at, datetime)
+
+        self.assertEqual(stored_amenity.created_at, amenity.created_at)
