d5a05cd6b538cc64dd6a68bf5b0f79887a1f2f96
David Zuokumor <144033027+dzuokumor@users.noreply.github.com>
2024-11-28 09:20:30 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,27 +4,43 @@ Fabric script based on the file 1-pack_web_static.py that distributes an
 archive to the web servers
 """
 
-from fabric.api import put, run, env
+from fabric import Connection
 from os.path import exists
-env.hosts = ['100.27.227.145', '54.226.121.12']
+from invoke import UnexpectedExit
 
+# Update the host IP addresses here
+env_hosts = ['34.229.14.248', '3.80.109.32']
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """Distributes an archive to the web servers."""
+    if not exists(archive_path):
         return False
     try:
         file_n = archive_path.split("/")[-1]
         no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+
+        for host in env_hosts:
+            conn = Connection(host)
+
+            # Upload the archive
+            conn.put(archive_path, '/tmp/')
+
+            # Create directory and extract the archive
+            conn.run(f'mkdir -p {path}{no_ext}/')
+            conn.run(f'tar -xzf /tmp/{file_n} -C {path}{no_ext}/')
+
+            # Remove the temporary archive
+            conn.run(f'rm /tmp/{file_n}')
+
+            # Move the contents and clean up
+            conn.run(f'mv {path}{no_ext}/web_static/* {path}{no_ext}/')
+            conn.run(f'rm -rf {path}{no_ext}/web_static')
+            conn.run(f'rm -rf /data/web_static/current')
+            conn.run(f'ln -s {path}{no_ext}/ /data/web_static/current')
+
         return True
-    except:
+    except UnexpectedExit as e:
+        print(f"Error during deployment: {e}")
         return False
+
